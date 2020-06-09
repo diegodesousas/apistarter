accbb935874974ee3b8cf06bb0dfbcea3122dbf6
Diego de Sousa <diegodesousas@yahoo.com.br>
2020-06-09 18:36:42 -0300

    remove media from ticket context

--- ticket/ticket.go
+++ ticket/ticket.go
@@ -1,9 +1,6 @@
 package ticket
 
-import "github.com/diegodesousas/apistarter/media"
-
 type Ticket struct {
-	ID     int64         `json:"id" db:"id"`
-	Name   string        `json:"name" db:"name"`
-	Medias []media.Media `json:"medias" db:"-"`
+	ID   int64  `json:"id" db:"id"`
+	Name string `json:"name" db:"name"`
 }
